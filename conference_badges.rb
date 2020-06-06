b985f66ee7be8d360761656b0cc642159df6ff11
Pedro Bravo <pedro.bravoa@yahoo.com>
2020-06-06 16:49:26 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,6 +13,6 @@ def assign_rooms(speakers)
 end
 
 def printer(speakers)
-  batch_badge_creator(speakers).each {|batch| print batch}
-  assign_rooms(speakers).each {|batch| print batch}
+  batch_badge_creator(speakers).each {|batch| puts batch}
+  assign_rooms(speakers).each {|batch| puts batch}
 end 
